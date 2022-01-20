baa21b6b595dfeb92f98b269065f2618487cfb4a
Erimias Siraye <88778321+erimias46@users.noreply.github.com>
2022-01-20 09:47:56 +0300

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,6 @@
+window.onclick = myFunction;
+
+// If the user clicks in the window, set the background color of <body> to yellow
+function myFunction() {
+  document.getElementsByTagName("BODY")[0].style.backgroundColor = "yellow";
+}
